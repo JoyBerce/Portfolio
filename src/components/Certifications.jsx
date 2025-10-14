f01e8398e2b44914b65ad1db830261f5a1fd676f
JoyBerce <bercenioj@gmail.com>
2025-10-14 19:26:18 +0800

    Updated

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/Home.css';
+import '../css/Cert.css';
 
 function Certifications() {
   return (
@@ -8,12 +8,41 @@ function Certifications() {
         <h2>Certifications and Badges</h2>
         <div className="certifications-grid">
           <div className="card certification-card">
-            <h3>React Developer Certification</h3>
-            <p>Certified by XYZ Academy, 2023</p>
+            <div className="certification-image">
+              <img src="../cert/cybersec.png" alt="Cybersec" />
+            </div>
+            <div className="cert-description">
+              <h3>Cybersecurity</h3>
+              <p>The purpose of obtaining an Information Technology Specialist Certification in Cybersecurity is to demonstrates that the earner has achieved competency in key areas such as network security, threats and vulnerabilities, and security best practices. It serves as a valuable credential for those pursuing careers in information security, enhancing employability and supporting further advancement in the IT field. </p>      
+          </div>
           </div>
           <div className="card certification-card">
-            <h3>JavaScript Fundamentals</h3>
-            <p>Certified by Codecademy, 2022</p>
+            <div className="certification-image">
+              <img src="../cert/networking.png" alt="Networking" />
+            </div>
+            <div className="cert-description">
+              <h3>Networking</h3>
+              <p>Earners of the IT Specialist Certification in Networking demonstrate validated expertise and practical skills in computer networking through an industry-recognized credential. This certification confirms that achievers have met the standards required in the IT networking field, supporting their career development and opening opportunities for advancement in the technology sector.</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Badges Section */}
+        <div className="badge-section">
+          <h3>Badges</h3>
+          <div className="badge-grid">
+            <div className="badge">
+              <img src="path_to_html_badge.jpg" alt="HTML5 Badge" />
+            </div>
+            <div className="badge">
+              <img src="path_to_css_badge.jpg" alt="CSS3 Badge" />
+            </div>
+            <div className="badge">
+              <img src="path_to_react_badge.jpg" alt="React Badge" />
+            </div>
+            <div className="badge">
+              <img src="path_to_js_badge.jpg" alt="JavaScript Badge" />
+            </div>
           </div>
         </div>
       </div>
